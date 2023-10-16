36a8e19bdfb2b9bdbdc8b3ff55c3b90ef41e91e5
JT Smith <nexxai@gmail.com>
2023-10-16 13:30:40 -0600

    Adding functionality and validation to exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,13 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+puts 'Enter a new store name:'
+@name = gets.chomp
+
+@store = Store.create(name: @name)
+
+@store.errors.each { |error| puts "#{error.full_message}" } if @store.valid? == false
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, :annual_revenue, presence: true
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_at_least_one_of_mens_and_or_womens_apparel
+
+  def must_carry_at_least_one_of_mens_and_or_womens_apparel
+    return if mens_apparel || womens_apparel
+
+    errors.add(:base,
+               'Must carry at least one of mens and/or womens apparel')
+  end
 end
