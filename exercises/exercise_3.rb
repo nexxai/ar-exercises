fb07fec477be36d8e7a7ea1adea98c298db74fd6
JT Smith <nexxai@gmail.com>
2023-10-16 12:44:35 -0600

    Adding functionality to exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,11 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
-puts "Exercise 3"
-puts "----------"
+puts 'Exercise 3'
+puts '----------'
 
 # Your code goes here ...
+@store3 = Store.last
+@store3.destroy
+
+puts "Stores: #{Store.count}"
